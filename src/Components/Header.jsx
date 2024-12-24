44f09d7e065d45667fde377ed759d59f69543afd
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2024-12-24 17:36:13 -0400

    Página de login feita

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import styles from "./Header.module.css"
+import Dogs from '../Assets/dogs.svg?react'
 
 export default function Header(){
     return(
-        <div className={styles.header}>
-            <nav className="container">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login / Criar</Link>
+        <header className={styles.header}>
+            <nav className={`${styles.nav} container`}>
+                <Link 
+                to="/" 
+                aria-label="Dos - Home"
+                className={styles.logo}
+                ><Dogs/>
+                </Link>
+                <Link 
+                to="/login"
+                className={styles.login}
+                >Login / Criar
+                </Link>
             </nav>
-        </div>
+        </header>
     )
 }
