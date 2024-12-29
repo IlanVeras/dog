db9ecd25b1aff51cd02bafac443971c370d07c35
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2024-12-29 17:01:09 -0400

    arquivo com endpoints com funções de login e de pegar token

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,34 +3,54 @@ import { Link } from "react-router-dom"
 import Input from "../Forms/Input"
 import Button from "../Forms/Button"
 import useForm from "../../Hooks/useForm"
+import { TOKEN_POST, USER_GET } from "../../api"
 
 export default function LoginForm(){
     const username = useForm()
     const password = useForm()
 
-
-    function handleSubmit(event) {
-        event.preventDefault();
+    /*
+     *useEffect usado para verificar se existe um token no local storage e chama a função getUser se existir
+     * @param {string} token - toke obtido do localStorage
+     * @return {void}
+     */
+    React.useEffect(() => {
+      const token = window.localStorage.getItem('token')
+      if(token){
+        getUser(token)
+      }
+      return
+    },[])
     
+
+    /*
+    *função que retorna um objeto com dados do usuáro a partir de um token
+    *@params {String} - token do localStorage
+    *response {Promise<Object>} @param: url => url de api.js
+    *json {Prmise<Object>} objeto com informações de usuário
+    */
+    async function getUser(token) {
+      const {url,options} = USER_GET(token)
+      const response = await fetch(url,options)
+      const json = await response.json()
+      console.log(json)
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
         if (username.validate() && password.validate()) {
-            console.log({username,password})
-          fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: username.value,
-                password: password.value,
-            }),            
+          const {url,options} = TOKEN_POST({
+            username:username.value,
+            password: password.value
           })
-            .then((response) => {
-              console.log(response);
-              return response.json();
-            })
-            .then((json) => {
-              console.log(json);
-            });
+          console.log({url,options})
+          const response = await fetch(url,options)
+          const json = await response.json()
+          console.log(response)
+          console.log(json)
+          //AQUI EU SALVO O TOKEN NO FRONT-END
+          window.localStorage.setItem('token', json.token)
+          getUser(json.token)
         }
     }
     return(
