eec8509ed9fbfa710dd3e5f9588841885a34e098
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-01-01 17:41:20 -0400

    problemas com a api e com onBlur

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import { UserContext } from "../../UserContext";
+import styles from "./login.module.css"
 
 export default function Login(){
     const {login} = React.useContext(UserContext)
@@ -13,13 +14,15 @@ export default function Login(){
         return <Navigate to="/conta"/>
     }
     return(
-        <div>
-            <Routes>
-                <Route path="/" element={<LoginForm/>}/>
-                <Route path="criar" element={<LoginCreate/>}/>
-                <Route path="perdeu" element={<LoginPasswordLost/>}/>
-                <Route path="resetar" element={<LoginPasswordReset/>}/>
-            </Routes>
-        </div>
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path="/" element={<LoginForm/>}/>
+                    <Route path="criar" element={<LoginCreate/>}/>
+                    <Route path="perdeu" element={<LoginPasswordLost/>}/>
+                    <Route path="resetar" element={<LoginPasswordReset/>}/>
+                </Routes>
+            </div>
+        </section>
     )
 }
--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import Input from "../Forms/Input";
+import useForm from "../../Hooks/useForm";
+import Button from "../Forms/Button";
+import { USER_POST } from "../../Api";
 
 export default function LoginCreate(){
+    const username = useForm()
+    const email = useForm('email')
+    const password = useForm()
+
+    async function handleSubmit(event){
+        event.preventDefault()
+        const { url, options } = USER_POST({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        });
+        const response = await fetch(url, options);
+        console.log(response)
+    }
+
     return(
-        <div>
-            Criar
-        </div>
+        <section className="animeLeft">
+            <h1 className="title">Cadastre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="Usuário" type="text" name="username" {...username}/>
+                <Input label="Email" type="text" name="username" {...email}/>
+                <Input label="Senha" type='text' name="password" {...password}/>
+                <Button>Cadastrar</Button>
+            </form>
+        </section>
     )
 }
