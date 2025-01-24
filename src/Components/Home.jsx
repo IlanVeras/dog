1b58c1c649b59aca4278fea13b5ac033c81ffe5e
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-01-24 13:15:04 -0400

    Loading
    usei um arquivo svg

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import UserPost from "../api/endpoints/UserPost";
 import Api from "../api/Api";
 import Feed from "./Feed/Feed";
+import Loading from "./Helper/Loading";
 
 export default function Home(){
     return(
         <section className="container mainContainer">
             <Feed/>
+            {/* <Loading/> */}
         </section>
     )
 }
