b59a9fe9dc74f3ca5670cd49605e85da87a67fab
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-01-25 20:58:17 -0400

    Sistema de perdeu a senha

--- src/api.js
+++ src/api.js
@@ -123,3 +123,31 @@ export function PHOTO_DELETE(id) {
       },
     };
 }
+
+
+//função para recuperar senha perdida
+export function PASSWORD_LOST(body){
+    return{
+        url: API_URL + '/api/password/lost',
+        options: {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        }
+    }
+}
+
+export function PASSWORD_RESET(body){
+    return{
+        url: API_URL + '/api/password/reset',
+        options: {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        }
+    }
+}
